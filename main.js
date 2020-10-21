1eab8416cf99bfc306c47d7404d2603f05df2745
Maximiliano Villasmil <mvillasmil00@gmail.com>
2020-10-21 18:05:37 -0400

    changed animation of title message to: from giant scale to normal. inserted gsap plugin scrolltrigger, registered it on main.js, and used it on .about

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
+gsap.registerPlugin(ScrollTrigger);
 let header_content = document.querySelector('.header-content')
 
 gsap.from(header_content, {
-    x: -200,
     opacity: 0,
-    duration: 0.7 
+    duration: 0.8,
+    scale: 3,
+    ease:"power4",
+});
+
+gsap.from('.about', {
+    scrollTrigger: ".about",
+    x:1000,
+    duration:1,
 });
